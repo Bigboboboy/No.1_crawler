f94259497851644e628b0d3888277c8a1c1eb999
Bigboboboy <2018211781@stu.cqupt.edu.cn>
2021-10-30 19:35:58 +0800

    Update to add a generator and packege the file_maker

--- jwzx_st_data.py
+++ jwzx_st_data.py
@@ -15,8 +15,7 @@ def get_main_page(url):
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
                     "Referer": "http://jwc.cqupt.edu.cn/kebiao/kb_stu.php?xh=2018211781",
                     "Upgrade-Insecure-Requests": "1",
-                    "Cookie": "_gscu_1026695571=63867326hevvfa19; vvjcgVbcZbczS=5UjfqIslYF54LY2N3mTP12hqnwTcLh_75PXs03OrqkK69fmXrrwLXt73XW7tfe_PZr6tDnzxgeG0UVVMYuLsU0q; mLvnBZTNP4mtS=5THlPEuRJEVOYGDGNgH2M7jIfkctzEaws8p5RH3qXzQFtIpbX2dyXUPQBmCM9.x9SpjZOw8xwt8HLOOgZqp0R6A; PHPSESSID=ST-699551-L5Y--FdS4sDBz1Ur1C0oObPpdKYauthserver2; mLvnBZTNP4mtT=53UIs1Dm606LqqqmZBrh8KqyYiuao1EkETNJyUS0QYvboDp6mhzYL.5otXw8YjF8jPAfSF9RzlP2YuJf2dFS3WuHrUpm2CBOWxmk_vbO0imB0wVd__SWVE39h.CSTQmY2S2COEaUN1X.uT7p0ADIxSMD7tfGcYFFvixDZ_E6l2jdC5KJiCZALC3a5r6s.IexfghZahKZKglqkyBvOtyd3wWcKrAF9xrDtIPLmCDi1TTngY_1id7wmex_ihKVvmkCEUJgrbiCFnjc1bzckv_gT5ZAcrC4pC0s8SVm5QxbVP9QW1uUGeUWid1024EOljOkFqLUyxpiOzE6vvZvDcVUBdg"
-                    }
+                    "Cookie": "_gscu_1026695571=63867326hevvfa19; vvjcgVbcZbczS=5UjfqIslYF54LY2N3mTP12hqnwTcLh_75PXs03OrqkK69fmXrrwLXt73XW7tfe_PZr6tDnzxgeG0UVVMYuLsU0q; mLvnBZTNP4mtS=5THlPEuRJEVOYGDGNgH2M7jIfkctzEaws8p5RH3qXzQFtIpbX2dyXUPQBmCM9.x9SpjZOw8xwt8HLOOgZqp0R6A; PHPSESSID=ST-141004-aDW6iOS6C84o6UmV1-IT94rIAqAauthserver2; mLvnBZTNP4mtT=53U0NYCmet3GqqqmZ0i2d5GLfqGTbu6SxuvK1N1XCDj3q6LUdiMP6.377cf1d9y.UzGUfu9vtFzO0KCTcYpgEDXNXcY7hZz6_Q5RoXYNu96G2Cfw4uJ6mcsYnvGS4d9cg19p_gpNCOddvyIkUAOeuA86FKOb5RfzO3aJADaf_kgTfAQ1zoh3XhjQK.4ctQ1Fd4Tv6eoFhCdrIS659KY.w0Pv5w6wWIRWoYPFGDWy1fQmPBrhYZ9Nvff037VCiA84ia59sD7MaGEweqGNIAqeYJXEV3F7V05e8pvvjf6WotDxG"}
     r = requests.get(url, headers=headers)
     print(r.status_code)
     if r.status_code == 200:
@@ -45,18 +44,19 @@ def parse_main_page(html):
             '选课状态': detail[10],
             '课程类别': detail[11],
         }
-        FILE.write(str(stu) + '\n')
+        yield stu
+
+
+def sav_to_file(data):
+    if not os.path.exists("./data"):
+        '''一个点退一级父目录'''
+        os.mkdir("./data")
+    with open("./data/jwzx_st_data.txt", 'a', encoding='utf-8') as f:
+        f.write(str(data) + '\n')
 
 
 if __name__ == '__main__':
     url = "http://jwc.cqupt.edu.cn/kebiao/kb_stuList.php?jxb=A00211A1040110001"
     html = get_main_page(url)
-    if not os.path.exists("../data"):
-        os.mkdir("../data")
-    try:
-        FILE = open("../data/jwzx_st_data.txt", 'w', encoding='utf-8')
-    except Exception as e:
-        print(e)
-        exit(-1)
-    parse_main_page(html)
-    FILE.close()
+    for i in parse_main_page(html):
+        sav_to_file(i)
